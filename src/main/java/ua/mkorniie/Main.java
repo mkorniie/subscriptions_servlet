7a1501ec2a2f86c760c68e3f6baf2779365ce2eb
mkorniie <->
2019-06-14 00:19:54 +0300

    test message bundle_working

---
+++ src/main/java/ua/mkorniie/Main.java
@@ -0,0 +1,24 @@
+package ua.mkorniie;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Main {
+    public static void main(String[] args) {
+        String language = "en";
+        String country = "US";
+
+        Locale locale = new Locale(language, country);
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("MessagesBundle", locale);
+
+        Locale localeFrench = new Locale("fr", "FR");
+        ResourceBundle resourceBundleF = ResourceBundle.getBundle("MessagesBundle_fr_FR", localeFrench);
+
+        String strUS = resourceBundle.getString("greetings");
+        System.out.println(strUS);
+
+        String strFr = resourceBundleF.getString("greetings");
+        System.out.println(strFr);
+
+    }
+}
